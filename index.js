7212db5cb5a215ede11e3625ec2378c17dc7d27c
Muriithi Ian <imuriithiian@gmail.com>
2022-08-23 09:55:12 +0300

    Working with selectors

---
+++ index.js
@@ -0,0 +1,14 @@
+const myTable = document.querySelector("table");
+console.log(myTable);
+
+const li2 = document.querySelector("ul.ranked-list li ul li");
+console.log(li2);
+
+const div4 = document.querySelector("ul.unranked-list li div");
+console.log(div4);
+
+const liList = document
+	.getElementById("app")
+	.querySelectorAll("ul.ranked-list li");
+
+console.log(liList);
